a7d9de31f2a1c1a2965a04d7c2d55568fbf7c53a
Ruchika Sanghi <sanghiruchika@gmail.com>
2026-09-02 22:44:29 -0500

    circles

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const tabs = document.querySelectorAll(".tab");
+const circles = document.querySelectorAll(".circle");
 const panels = document.querySelectorAll(".panel");
 
-tabs.forEach((tab) => {
-  tab.addEventListener("click", () => {
-    tabs.forEach((t) => t.classList.remove("is-active"));
+circles.forEach((circle) => {
+  circle.addEventListener("click", () => {
+    circles.forEach((c) => c.classList.remove("is-active"));
     panels.forEach((p) => p.classList.remove("is-active"));
 
-    tab.classList.add("is-active");
-    document.getElementById(tab.dataset.tab).classList.add("is-active");
+    circle.classList.add("is-active");
+    document.getElementById(circle.dataset.tab).classList.add("is-active");
   });
 });
